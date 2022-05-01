bfe01b8ad92e79f582903b025986e3954e8f0041
Robert Adams <misterblue@misterblue.com>
2022-04-30 20:32:51 -0600

    Remove lurking references to System.Numerics

--- CommonEntities/GltfClasses.cs
+++ CommonEntities/GltfClasses.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.IO;
 
 //RA using SharpGLTF.Schema2;
--- CommonEntitiesConv/BConverterOS.cs
+++ CommonEntitiesConv/BConverterOS.cs
@@ -15,7 +15,6 @@
  */
 
 using System;
-using System.Numerics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
