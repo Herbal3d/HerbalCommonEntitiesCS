bf7a86db85454296fa398f136f234672eb221352
Robert Adams <misterblue@misterblue.com>
2024-01-30 18:30:06 +0000

    Merge dotnet6 into master. Bump version numbers.

--- CommonEntities/VersionInfo.cs
+++ CommonEntities/VersionInfo.cs
@@ -3,8 +3,8 @@
 using System;
 namespace org.herbal3d.cs.CommonEntities {
     public class VersionInfo {
-        public static string appVersion = "2.2.3";
-        public static string longVersion = "2.2.3-20230729-4aa26f22";
-        public static string buildDate = "20230729";
+        public static string appVersion = "2.2.4";
+        public static string longVersion = "2.2.4-20240130-9a8756d4";
+        public static string buildDate = "20240130";
     }
 }
--- CommonEntitiesConv/VersionInfo.cs
+++ CommonEntitiesConv/VersionInfo.cs
@@ -3,8 +3,8 @@
 using System;
 namespace org.herbal3d.cs.CommonEntitiesConv {
     public class VersionInfo {
-        public static string appVersion = "2.2.2";
-        public static string longVersion = "2.2.2-20230729-4aa26f22";
-        public static string buildDate = "20230729";
+        public static string appVersion = "2.2.3";
+        public static string longVersion = "2.2.3-20240130-9a8756d4";
+        public static string buildDate = "20240130";
     }
 }
